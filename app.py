bd813c0c0b3bd2707d3e1d44b0ed437ed817ca0d
Amruta Ghate <amruta.ghate@gmail.com>
2017-03-07 00:19:14 -0500

    changes

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+from flask_login import AnonymousUserMixin
+from okta import SessionsClient
 import requests
 import json
 
@@ -13,7 +15,6 @@ app = Flask(__name__)
 
 base_url =  'https://dev-788346.oktapreview.com'
 api_token = '00AQM7JlAklG5P-ipn2zwuXtQNDZlkAfkFkA0o6ayy'
-okta = {}
 
 headers = {
     # "Authorization" is only needed for social transaction calls
@@ -21,13 +22,18 @@ headers = {
     'Authorization': 'SSWS {}'.format(api_token),
 }
 
-public_keys = {}
-allowed_domains = ['okta.com', 'oktapreview.com']
+sessionsClient = SessionsClient(base_url, api_token)
+
 app.secret_key = api_token
 
 login_manager = LoginManager()
 login_manager.setup_app(app)
 
+class Anonymous(AnonymousUserMixin):
+  def __init__(self):
+    self.user_id = 'Guest'
+
+login_manager.anonymous_user = Anonymous
 
 class UserSession:
     def __init__(self, user_id):
@@ -52,18 +58,48 @@ def load_user(user_id):
 	print "Loading user: " + user_id
 	return UserSession(user_id)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('login.html') 
+	if (request.method == 'GET' and current_user.user_id != "Guest"):
+		url = url_for('logged_in', _external=True)
+		return redirect(url)
+		
+	elif request.method == 'GET':
+		return render_template('login.html')
 
-@app.route('/loggedin')
-#@login_required
+	opts = {}
+
+	session = None
+	try:
+		session = sessionsClient.create_session(
+			request.form['username'],
+			request.form['password'])
+	except Exception, e:
+		print e
+		opts['invalid_username_or_password'] = True
+		return render_template('logged_in.html', opts=opts)
+
+	user = UserSession(session.userId)
+	login_user(user)
+	url = url_for('logged_in', _external=True)
+	return redirect(url)
+
+
+@app.route('/logged_in')
+@login_required
 def logged_in():
-#	print current_user
-#	opts = {'user': current_user}
-	return render_template('loggedin.html') #,
-#		opts=opts,
-#		okta=okta)
+	print current_user.is_authenticated
+	opts = {'user': current_user}
+	user_id_login = current_user.user_id
+	authn_url = "{}/api/v1/users/{}".format(base_url, user_id_login)
+	r = requests.get(authn_url, headers=headers)
+	result = r.json()
+	return render_template('logged_in.html', opts = opts, result = result)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
@@ -73,17 +109,20 @@ def registration():
 		r = requests.post(authn_url, headers=headers, data=json.dumps(payload))
 		result = r.json()
 		username = request.form['firstName']
-		return render_template('createuser.html', user=username)
+		return render_template('create_user.html', user=username)
 	return render_template('registration.html')
 
-@app.route('/adminlistusers', methods=['GET'])
+@app.route('/admin_list_users', methods=['GET'])
+@login_required
 def listusers():
+	opts = {'user': current_user}
 	authn_url = "{}/api/v1/users?limit=25".format(base_url)
 	r = requests.get(authn_url, headers=headers)
 	result = r.json()
-	return render_template('adminlistusers.html', results=result)
+	return render_template('admin_list_users.html', opts = opts, results=result)
 
-@app.route('/deleteuser', methods=['GET','POST'])
+@app.route('/delete_user', methods=['GET','POST'])
+@login_required
 def deleteuser():
 	if (request.method  == 'POST'):
 		login = request.form['login']
@@ -93,13 +132,15 @@ def deleteuser():
 		user_id = result[0]['id']
 		authn_url2 = "{}/api/v1/users/{}/lifecycle/deactivate".format(base_url, user_id)
 		r = requests.post(authn_url2, headers=headers)
-		return redirect('/adminlistusers')
+		return redirect('/admin_list_users')
+	opts = {'user': current_user}
 	authn_url = "{}/api/v1/users?limit=25".format(base_url)
 	r = requests.get(authn_url, headers=headers)
 	result = r.json()
-	return render_template('deleteuser.html', results=result)
+	return render_template('delete_user.html', results=result, opts = opts)
 
-@app.route('/changeuser', methods=['GET', 'POST'])
+@app.route('/change_user', methods=['GET', 'POST'])
+@login_required
 def changeuser():
 	if (request.method  == 'POST'):
 		login = request.form['login']
@@ -109,11 +150,12 @@ def changeuser():
 		user_id = result[0]['id']
 		authn_url2 = "{}/api/v1/users/{}/lifecycle/reset_password?sendEmail=false".format(base_url, user_id)
 		r = requests.post(authn_url2, headers=headers)
-		return redirect('/adminlistusers')
+		return redirect('/admin_list_users')
+	opts = {'user': current_user}
 	authn_url = "{}/api/v1/users?limit=25".format(base_url)
 	r = requests.get(authn_url, headers=headers)
 	result = r.json()
-	return render_template('modifyuser.html', results=result)
+	return render_template('modify_user.html', results=result, opts = opts)
 
 
 # start the server with the 'run()' method
